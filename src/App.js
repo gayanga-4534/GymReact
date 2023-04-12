c76a46d9591d98b4311f3b706c141f0154bc5f81
gayanhasandaruwan@gmail.com <gayanhasandaruwan@gmail.com>
2023-04-12 16:23:35 +0530

    Reasons section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Hero from './components/Hero';
+import Reasons from './components/Reasons/Reasons';
 import Programs from './components/programs/Programs';
 function App() {
   return (
     <div className="App">
          <Hero/>
          <Programs/>
+         <Reasons/>
     </div>
   );
 }
