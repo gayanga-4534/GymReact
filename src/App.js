0d90cc9e8102032351cf2aa5725b8e1758e366b3
gayanhasandaruwan@gmail.com <gayanhasandaruwan@gmail.com>
2023-04-20 11:22:52 +0530

    Testimonials

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Hero from './components/Hero';
+import Plans from './components/Plans/Plans';
 import Reasons from './components/Reasons/Reasons';
+import Testimonials from './components/Testimonials/Testimonials';
 import Programs from './components/programs/Programs';
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
          <Hero/>
          <Programs/>
          <Reasons/>
+         <Plans/>
+         <Testimonials/>
     </div>
   );
 }
